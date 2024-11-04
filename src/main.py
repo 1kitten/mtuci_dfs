edecf00e0f90f0161cb4b05916be77ab1087fd14
Maksim Strelchenko <maksim.strelchenko99@gmail.com>
2024-11-04 14:48:45 +0300

    f

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
 from district.router import dist_router
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI(title="dsf")
 app.include_router(router=dist_router, prefix="/district")
-
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
